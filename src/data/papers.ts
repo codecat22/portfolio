aa4cbfe5b875a2881f78b529c67d70a5671fa702
codecat20 <siyana.ivanova13@gmail.com>
2022-01-14 18:45:34 +0100

    papers with descriptions

--- src/data/papers.ts
+++ src/data/papers.ts
@@ -1,9 +1,72 @@
 export default [
 	{
 		name: 'AestheticsOnline',
-		date: '1 September 2020',
-		title: 'Cat paper very title much good',
+		date: 'January 2021',
+		title: 'Big Mood',
 		abstract:
-			'Cat text very excerpt wow, such many text is shown here. This is some cat text for sure. Cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat catCat text very excerpt wow, such many text is shown here. This is some cat text for sure. Cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat catCat text very excerpt wow, such many text is shown here. This is some cat text for sure. Cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat catCat text very excerpt wow, such many text is shown here. This is some cat text for sure. Cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat catCat text very excerpt wow, such many text is shown here. This is some cat text for sure. Cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat catCat text very excerpt wow, such many text is shown here. This is some cat text for sure. Cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat catCat text very excerpt wow, such many text is shown here. This is some cat text for sure. Cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat catCat text very excerpt wow, such many text is shown here. This is some cat text for sure. Cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat catCat text very excerpt wow, such many text is shown here. This is some cat text for sure. Cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat cat'
+			'This paper focuses on the role of design tools used in professional settings, moodboards in particular, in online spaces created and curated by non-designers around a particular atmosphere and a desired emotional disposition or appeal.'
+	},
+	{
+		name: 'StandingOnYourFeet',
+		date: 'September 2021',
+		title: 'Standing On Your Feet',
+		abstract:
+			'Elevators can be associated with boredom and waiting. We attempt to alleviate some of this expected boredom by changing up the way one interacts with an elevator and bringing attention to the related somatic experiences that normally go unnoticed'
+	},
+	{
+		name: 'DarkPatterns',
+		date: 'September 2020',
+		title: 'Dark Patterns in Web Design',
+		abstract:
+			'Discussing the ethics of design practices using the example of “dark patterns” in contemporary web design: features and choices which aim to deceive the user in order to achieve a goal related to corporate or political profit.'
+	},
+	{
+		name: 'FurhatCulinaryInstructor',
+		date: 'January 2021',
+		title: 'Furhat Culinary Instructor',
+		abstract:
+			'This report outlines the details of a project and accompanying pilot study that focused on the effect of using a Furhat Culinary Instructor helper robot when learning a new recipe.'
+	},
+	{
+		name: 'PDPVWebSystemThesis',
+		date: 'January 2020',
+		title: 'Precipitation Data Processing and Visualisation Web System',
+		abstract:
+			'This document presents the results of our thesis project: an automatic web system for downloading, processing, and visualizing precipitation data obtained via NASA’s GPM Mission.'
+	},
+	{
+		name: 'WorkbookCloud',
+		date: 'October 2020',
+		title: 'Cloud Connected',
+		abstract:
+			'A workbook presenting a design concept for a productivity system centered around a smart desktop lamp called "The Cloud."'
+	},
+	{
+		name: 'StyleTransferMLW',
+		date: 'June 2019',
+		title: 'Style Transfer from Artworks to Photographs',
+		abstract:
+			'Presenting a program for style transfer from one image to another, using two images as input ("style" and "content") and outputting a filtered version of the content image, changed to emulate the chosen style.'
+	},
+	{
+		name: 'TransphobiaClassifierNLP',
+		date: 'May 2019',
+		title: 'Transphobic Speech Classifier',
+		abstract:
+			'In this project, our goal was to observe the prevalence of transphobic content online, while also applying natural language processing methods to classify a specific “subgenre” of hate speech in practice.'
+	},
+  {
+		name: 'ScienceEducation',
+		date: 'December 2020',
+		title: 'Science Education vs Learning Science',
+		abstract:
+			'A short essay discussing the flaws and pitfalls of contemporary education in the sciences and how the experience of students could improve.'
+	},
+  {
+		name: 'YunakDemoScript',
+		date: 'May 2021',
+		title: 'Script for the Yunak game demo',
+		abstract:
+			'A draft of the script to a story=based puzzle game based around Bulgarian mythology - Yunak.'
 	}
 ];
